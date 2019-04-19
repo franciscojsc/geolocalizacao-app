8de0a8a77c21f78202284952eb53d33c6356ca97
Francisco Chaves <francisco.jschaves@gmail.com>
2019-04-19 20:27:32 -0300

    Classe que representa o retorno da API

---
+++ app/src/main/java/br/com/franciscochaves/geolocalizaoip/Localizacao.java
@@ -0,0 +1,61 @@
+package br.com.franciscochaves.geolocalizaoip;
+
+public class Localizacao {
+
+    //{"as":"AS53057 RedeHost Internet Ltda.","city":"Gravataí","country":"Brazil","countryCode":"BR","isp":"RedeHost Internet Ltda","lat":-29.9423,"lon":-50.9908,"org":"RedeHost Internet Ltda","query":"177.55.104.82","region":"RS","regionName":"Rio Grande do Sul","status":"success","timezone":"America/Sao_Paulo","zip":"94000-000"}
+
+
+    private String countryCode;
+    private String country;
+    private String city;
+    private String region;
+    private String zip;
+
+    public Localizacao(String countryCode, String country, String city, String region, String zip) {
+        this.countryCode = countryCode;
+        this.country = country;
+        this.city = city;
+        this.region = region;
+        this.zip = zip;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
